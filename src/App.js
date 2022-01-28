e12706c3da9e16f2bea4f4d77ba2b0eb2cd8a603
saiful-dev <saiful.hstu.sii@gmail.com>
2022-01-28 18:02:39 +0600

    add two box and dropeZone

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react"
 import { useDropzone } from "react-dropzone"
 import Box from '@mui/material/Box';
+
+
+import { Grid } from "@mui/material";
+
 import './App.css'
-import { height } from "@mui/system";
 
 function App() {
-  const [files, setFiles] = useState([])
+  const [files, setFiles] = useState([]);
+  const [files2, setFiles2] = useState([])
 
 
   
-  const { getRootProps, getInputProps } = useDropzone({
+  const { getRootProps:getRootFile1, getInputProps:getInputFile1 } = useDropzone({
     accept: "image/*",
     onDrop: (acceptedFiles) => {
       setFiles(
@@ -20,28 +24,73 @@ function App() {
         )
       )
     },
-  })
+  
+  });
 
-  const images = files.map((file) => (
+  const { getRootProps:getRootFile2, getInputProps:getInputFile2 } = useDropzone({
+    accept: "image/*",
+    onDrop: (acceptedFiles) => {
+      setFiles2(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      )
+    },
+  
+  });
+
+
+  
+
+  
+ 
+
+  const images1 = files.map((file) => (
     <div key={file.name}>
       <div>
-        <img src={file.preview} style={{ width: "200px", height:"300px"}} alt="preview" />
+        <img src={file.preview} style={{ width: "50%", height:"200px"}} alt="preview" />
       </div>
     </div>
   ))
 
+  const images2 = files2.map((file) => (
+    <div key={file.name}>
+      <div>
+        <img src={file.preview} style={{ width: "50%", height:"200px"}} alt="preview" />
+      </div>
+    </div>
+  ))
   return (
     <React.Fragment>
-    <div className="box">
-      <div {...getRootProps()}>
-        <input {...getInputProps()} />
-        <button>choose image</button>
-      </div>
-      <div>{images}</div>
 
-      
+    <Grid container spacing={2} columns={16}>
+
+      <Grid item xs={8}>
+          <div className="box">
+              <div {...getRootFile1()}>
+                  <input {...getInputFile1()} />
+                  <button>choose image</button>
+              </div>
+              <div>{images1}</div>
+          </div>
+      </Grid>
+
+
+    <Grid item xs={8}>
+        <div className="box">
+            <div {...getRootFile2()}>
+                <input {...getInputFile2()} />
+                <button>choose image</button>
+            </div>
+             <div>{images2}</div>
+        </div>
+    </Grid>
+
+  </Grid>
+
     
-   </div>
    </React.Fragment>
   )
 }
